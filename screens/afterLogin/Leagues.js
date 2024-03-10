d23c445f66cbe4afe1617649552751fdcf91aee7
Christopher <christopher.geever@gmail.com>
2024-03-09 22:12:43 -0500

    small banner

--- screens/afterLogin/Leagues.js
+++ screens/afterLogin/Leagues.js
@@ -2,14 +2,39 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native';
 import BannerBig from '../../components/BannerBig';
+import BannerSmall from '../../components/BannerSmall';
+import LeagueData from '../../dummy_data/Leagues_Data.json';
 
 export default function Leagues(){
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.heading}>Upcoming Leagues</Text>
-      <BannerBig />
+      {
+        LeagueData.map((league) => {
+          return league.Status==="Upcoming"? <BannerBig key={league.heading} banner_data={league} /> :null
+        })
+      }
+
+      <Text style={{marginBottom:"5%"}} />
+
       <Text style={styles.heading}>Ongoing Leagues</Text>
+      {
+        LeagueData.map((league) => {
+          return league.Status==="Playing"? <BannerSmall key={league.heading} banner_data={league} /> :null
+        })
+      }
+
+      <Text style={{marginBottom:"5%"}} />
+
       <Text style={styles.heading}>Previous Leagues</Text>
+      {
+        LeagueData.map((league) => {
+          return league.Status==="Overdue"? <BannerSmall key={league.heading} banner_data={league} /> :null
+        })
+      }
+
+      <Text style={{marginBottom:"20%"}} />
+      
     </ScrollView>
   );
 };
@@ -19,8 +44,8 @@ const styles = StyleSheet.create({
     flex: 1,
     //justifyContent: 'center',
     //alignItems: 'center',
-    paddingHorizontal:"8%",
-    paddingVertical:"5%"
+    paddingHorizontal:"5%",
+    paddingVertical:"5%",
   },
   heading: {
     fontSize: 23,
