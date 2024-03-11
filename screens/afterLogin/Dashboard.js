80854a7bf88e6c38495141d78470e39128426976
Anisha Pant <annray@Moon.local>
2024-03-11 11:25:52 -0400

    Add upcoming league feature

--- screens/afterLogin/Dashboard.js
+++ screens/afterLogin/Dashboard.js
@@ -1,21 +1,27 @@
-// HeadingScreen.js
-
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native';
 
-export default function Dashboard(){
+import { status } from '../../constants/constant';
+import LeagueData from '../../dummy_data/Leagues_Data.json';
+import BannerBig from '../../components/BannerBig';
+
+export default function Dashboard() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Dashboard</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.heading}>Upcoming Leagues</Text>
+      {
+        LeagueData.map((league) => {
+          return league.Status === status.UPCOMING ? <BannerBig key={league.heading} banner_data={league} /> : null
+        })
+      }
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   heading: {
     fontSize: 24,
