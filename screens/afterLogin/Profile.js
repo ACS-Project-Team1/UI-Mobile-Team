a8a484856ebf11b22e6e2f32bdb95732ea7d829f
Anisha Pant <annray@Moon.local>
2024-04-19 20:33:34 -0400

    Implement Profile screen

--- screens/afterLogin/Profile.js
+++ screens/afterLogin/Profile.js
@@ -1,29 +1,88 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { ButtonText, StyledButton } from '../../components/styles';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Card } from 'react-native-elements';
 
-export default function Profile({navigation}){
+import { MyTextInput } from '../../components/MyTextInput';
+import { InnerContainer } from '../../components/styles';
+
+export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Profile</Text>
-      <StyledButton onPress={() => navigation.navigate("Login")}>
-        <ButtonText>
-            Logout
-        </ButtonText>
-      </StyledButton>
+      <Card containerStyle={styles.cardContainer}>
+        <View style={styles.cardContent}>
+          <View style={styles.avatarContainer}>
+            <Image
+              source={{
+                uri: 'https://img.freepik.com/free-photo/business-woman-posing-street_23-2148213512.jpg?w=2000&t=st=1713571764~exp=1713572364~hmac=184f3fb01492a6cd6d2cb8c93c373e4ff10c049806deef86939276c41f86b9b8',
+              }}
+              style={styles.avatar}
+            />
+          </View>
+          <View style={styles.userInfoContainer}>
+            <Text style={styles.userId}>Player ID #123</Text>
+            <Text style={styles.username}>@annArray</Text>
+          </View>
+        </View>
+      </Card>
+      <InnerContainer>
+        <View style={styles.detailsContainer}>
+          <MyTextInput
+            label="First Name"
+            placeholder="Ann"
+            value="Ann"
+            editable={false}
+          />
+          <MyTextInput
+            label="Last Name"
+            placeholder="Array"
+            value="Array"
+            editable={false}
+          />
+          <MyTextInput
+            label="Email Address"
+            placeholder="anna@gmail.com"
+            value="anishapant@gmail.com"
+            keyboardType="email-address"
+            editable={false}
+          />
+        </View>
+      </InnerContainer>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  cardContainer: {
+    padding: 20,
+    borderRadius: 10,
+    elevation: 3,
+    marginBottom: 20,
+  },
+  cardContent: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
-  heading: {
-    fontSize: 24,
+  avatarContainer: {
+    marginRight: 20,
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  detailsContainer: {
+    paddingTop: 30,
+  },
+  userId: {
+    fontSize: 20,
     fontWeight: 'bold',
   },
+  username: {
+    fontSize: 16,
+  },
 });
-
