8176bd49bac5b58d90f73ad1e86adc0bc20dd305
Christopher <christopher.geever@gmail.com>
2024-04-21 22:12:09 -0400

    integrated

--- screens/afterLogin/teams/CreateTeam.js
+++ screens/afterLogin/teams/CreateTeam.js
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button, StyleSheet, Text } from "react-native";
 import { View } from "react-native";
 import { TextInput } from "react-native";
 import CustomButton from "../../../components/CustomButton";
 import { Colors } from "../../../components/styles";
 import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../../context/AuthProvider";
+import BaseRequest from "../../../constants/requests";
+import { BASE_URL } from "../../../constants/constant";
 
 export default function CreateTeam(){
     const [teamName, setTeamName] = useState('');
     const [dpUrl, setDpUrl] = useState('');
-    const [player1Id, setPlayer1Id] = useState('');
     const [player2Id, setPlayer2Id] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleSubmit = () => {
+    const {userId} = useContext(AuthContext)
+
+    const handleSubmit = async () => {
         // Handle form submission here
+        if (!teamName || !dpUrl || !userId || !player2Id) {
+            setErrorMessage("All fields are required");
+            return; // Stop execution if any field is empty
+        }
+        setErrorMessage('')
         console.log("Form submitted");
+        const req_data = {
+            "teamName":teamName,
+            "dpUrl":dpUrl,
+            "players":[userId, player2Id]
+        }
+
+        const response = await BaseRequest.post(`${BASE_URL}/teams/createTeam`,req_data)
+        console.log(response.status)
+        if(response.status===201){
+            navigation.navigate('teamsList')
+        }
     };
 
     const navigation = useNavigation();
@@ -37,8 +58,8 @@ export default function CreateTeam(){
             <Text>Player 1 ID:</Text>
             <TextInput
                 style={styles.input}
-                value={player1Id}
-                onChangeText={setPlayer1Id}
+                value={userId}
+                editable={false}
             />
 
             <Text>Player 2 ID:</Text>
@@ -47,8 +68,9 @@ export default function CreateTeam(){
                 value={player2Id}
                 onChangeText={setPlayer2Id}
             />
+            {errorMessage!=='' && <Text style={{color:'red'}}></Text>}
             <View style={styles.buttonContainer}>
-                <CustomButton text="SUBMIT" color={Colors.theme} onPress={()=>navigation.navigate('teamsList')}/>
+                <CustomButton text="SUBMIT" color={Colors.theme} onPress={handleSubmit}/>
             </View>
             
         </View>
