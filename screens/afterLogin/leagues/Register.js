bd4136b73d7f1d43b5bff8d0cea2a90e89953ac4
Anisha Pant <annray@Moon.local>
2024-04-22 13:10:31 -0400

    API integration for league registeratio

--- screens/afterLogin/leagues/Register.js
+++ screens/afterLogin/leagues/Register.js
@@ -10,159 +10,181 @@ import { Ionicons } from "@expo/vector-icons"
 import Teams from '../../../dummy_data/Teams_Data.json'
 import RNPickerSelect from "react-native-picker-select";
 import { AuthContext } from "../../../context/AuthProvider";
+import BaseRequest from "../../../constants/requests";
+import { BASE_URL } from "../../../constants/constant";
 
 
-export default function RegisterLeague(){
+export default function RegisterLeague() {
 
-    const [teamName, setTeamName] = useState("");
-    
-    const handleSubmit = () => {
-        // Handle form submission here
-        console.log("Form submitted");
-    };
+  const [teamName, setTeamName] = useState(null);
+  const [selectedteamId, setSelectedTeamId] = useState(null)
 
-    
+  const [yourTeams, setYourTeams] = useState([])
+  const isFocused = useIsFocused();
+  const { userId } = useContext(AuthContext)
 
-      const [yourTeams, setYourTeams] = useState([])
-      const isFocused = useIsFocused();
-      const {userId} = useContext(AuthContext)
-    
-      useEffect(()=> {
-        const fetchData = async () => {
-          try {
-            const response = await BaseRequest.get(`${BASE_URL}/users/getTeams/${userId}`);
-            console.log(response.data)
-            setYourTeams(response.data)
-          } catch (error) {
-            console.error("Error fetching leagues:", error);
-          }
-        }
-    
-        if (isFocused) {
-          fetchData();
-        }
-      }, [isFocused]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await BaseRequest.get(`${BASE_URL}/users/getTeams/${userId}`);
 
-    const teamItems = yourTeams.map((team,index) => ({
-      label: team.teamName,
-      value: JSON.stringify(team),
-    }));
+        setYourTeams(response.data)
+      } catch (error) {
+        console.error("Error fetching leagues:", error);
+      }
+    }
 
-    const navigation = useNavigation()
-    const route = useRoute()
-    const league_data = route.params
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
 
-    return(
-        <View style={styles.container}>
-            <Text style={styles.heading}>Register</Text>
+  const teamItems = yourTeams.map((team, index) => ({
+    label: team.teamName,
+    value: JSON.stringify(team),
+  }));
 
-            <View style={[styles.card, styles.shadowProp]}>
-                <View style={styles.inline}>
-                    <Text style={styles.heading}>{league_data.heading}</Text>
-                    <Text style={styles.subheading}><Ionicons name={"calendar"} size={20} />{league_data.Duration}</Text>
+  const navigation = useNavigation()
+  const route = useRoute()
+  const league_data = route.params
 
-                </View>
-                <Text style={styles.subheading}>{league_data.location}</Text>
-                
 
-                
-                    <RNPickerSelect
-                    
-                        value={teamName}
-                        onValueChange={(value) => setTeamName(value)}
-                        items={teamItems}
-                        style={pickerSelectStyles}
-                        placeholder={{label:'Select a Team', value:''}}
-                    />
-                    
-            
-            </View>
-            
-            <View style={styles.buttonContainer}>
-                <CustomButton text="SUBMIT" color={Colors.theme} onPress={()=>navigation.navigate('Leaguesmain')}/>
-            </View>
-            
+
+  const handleSelected = (value) => {
+    if (value) {
+      const valueParsed = JSON.parse(value);
+      setSelectedTeamId(valueParsed?.teamId)
+
+
+      setTeamName(value)
+
+    }
+
+
+  }
+
+  const handleOnSubmit = async () => {
+    try {
+      await BaseRequest.post(`${BASE_URL}/leagues/joinLeague/${league_data?.leagueId}/${selectedteamId}`);
+      navigation.navigate('Leaguesmain')
+
+    } catch (error) {
+      console.error("Error joining league", error);
+    }
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Register</Text>
+
+      <View style={[styles.card, styles.shadowProp]}>
+        <View style={styles.inline}>
+          <Text style={styles.heading}>{league_data.heading}</Text>
+          <Text style={styles.subheading}><Ionicons name={"calendar"} size={20} />{league_data.Duration}</Text>
+
         </View>
-    )
+        <Text style={styles.subheading}>{league_data.location}</Text>
+
+
+
+        <RNPickerSelect
+
+          value={teamName}
+          onValueChange={handleSelected}
+          items={teamItems}
+          style={pickerSelectStyles}
+          placeholder={{ label: 'Select a Team', value: '' }}
+        />
+
+
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <CustomButton text="SUBMIT" color={Colors.theme} onPress={() => handleOnSubmit()} />
+      </View>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        padding:'7%',
-        backgroundColor:'white'
-    },
-    card: {
-        backgroundColor:'white',
-        marginVertical:"3%",
-        marginHorizontal:"0%",
-        borderColor:'#B1B2B9',
-        borderRadius:"10%",
-        padding:'2%',
-        paddingHorizontal:'4%',
-        height:180
-      },
-    heading: {
-        fontSize: 25,
-        fontWeight: 'bold',
-      },
-      subheading: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color:'gray'
-      },
-      input: {
-        height: 50,
-        borderRadius:'10px',
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingHorizontal: 10,
-        backgroundColor:Colors.secondary,
-        borderColor:Colors.secondary
-      },
-      buttonContainer:{
-        alignItems:'center',
-        
-      },
-      shadowProp: {
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-        elevation: 5,
-      },
-      inline:{
-        flexDirection:'row', flexWrap:'wrap',
-        justifyContent:'space-between',
-    },
-    picker: {
-        height: 40,
-        marginBottom: 10,
-      },
-      
+  container: {
+    padding: '7%',
+    backgroundColor: 'white'
+  },
+  card: {
+    backgroundColor: 'white',
+    marginVertical: "3%",
+    marginHorizontal: "0%",
+    borderColor: '#B1B2B9',
+    borderRadius: "10%",
+    padding: '2%',
+    paddingHorizontal: '4%',
+    height: 180
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  subheading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gray'
+  },
+  input: {
+    height: 50,
+    borderRadius: '10px',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    backgroundColor: Colors.secondary,
+    borderColor: Colors.secondary
+  },
+  buttonContainer: {
+    alignItems: 'center',
+
+  },
+  shadowProp: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  inline: {
+    flexDirection: 'row', flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  picker: {
+    height: 40,
+    marginBottom: 10,
+  },
+
 })
 
 const pickerSelectStyles = {
-    inputIOS: {
-      
-      fontSize: 25,
-      borderColor: "#B1B2B9",
-      borderWidth: 1,
-      marginVertical: 5, // Changed from "5%" to 5
-      padding: 5, // Changed from "5%" to 5
-      borderRadius: 10,
-      // Add or override iOS styles here
-    },
-    inputAndroid: {
-      ...RNPickerSelect.defaultProps.inputAndroid,
-      // Add or override Android styles here
-    },
-    placeholder: {
-      color: 'gray', // Placeholder color
-      fontSize: 25, // Placeholder font size
-    },
-    value: {
-        ...RNPickerSelect.defaultProps.value,
-        color: 'black', // Placeholder color
-        fontSize: 25, // Placeholder font size
-      },
-  };
+  inputIOS: {
+
+    fontSize: 25,
+    borderColor: "#B1B2B9",
+    borderWidth: 1,
+    marginVertical: 5, // Changed from "5%" to 5
+    padding: 5, // Changed from "5%" to 5
+    borderRadius: 10,
+    // Add or override iOS styles here
+  },
+  inputAndroid: {
+    ...RNPickerSelect.defaultProps.inputAndroid,
+    // Add or override Android styles here
+  },
+  placeholder: {
+    color: 'gray', // Placeholder color
+    fontSize: 25, // Placeholder font size
+  },
+  value: {
+    ...RNPickerSelect.defaultProps.value,
+    color: 'black', // Placeholder color
+    fontSize: 25, // Placeholder font size
+  },
+};
